58c76bae4ef1133841bc71b97a822cbdf1502784
Aaron Lim <aronlimjm@gmail.com>
2022-07-04 16:48:43 +0800

    Minimal Working Example

--- app.R
+++ app.R
@@ -26,36 +26,48 @@ ui <- fluidPage(
     fluidRow(column(3, verbatimTextOutput("stimuli"))),
     # fluidRow(column(3, verbatimTextOutput("nex"))),
     # numericInput("ans", label = h3("Answer"), value = 0),
-    uiOutput("test"),
+    uiOutput("ansbox"),
     fluidRow(column(3, verbatimTextOutput("allans")))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     answers <- list()
+    output$stimuli <- renderPrint({"INSTRUCTIONS"})
     
-    tohide <- reactiveTimer(intervalMs = 5000)
+    
+    tohide2 <- reactiveTimer(intervalMs = 7000)
+    params <- reactiveValues(counter = 0)
+    
+    observeEvent(input$nex,{
+        params$counter <- 0
+        output$ansbox <- NULL
+        output$stimuli <- renderPrint({rand_df[input$nex]})
+        print("next")
+        answers[[input$nex]] <<- input$ans
+        output$allans <- renderPrint({answers})
+        
+        tohide <<<- reactiveTimer(intervalMs = 6000)
+    })
     
     observeEvent(tohide(),
                  {
-                     
-                     output$stimuli <- renderPrint({NULL}) 
-                     output$test <- renderUI({
-                         numericInput("ans", label = h3("Answer"), value = 0)
-                     })
-                     
-                 })
+                     print(params$counter)
+                     if(input$nex != 0){
+                         output$stimuli <- renderPrint({"HIDDEN"})
+                         if(params$counter == 0){
+                             output$ansbox <- renderUI({
+                                 numericInput("ans", label = h3("Answer"), value = 0)})
+                         }
+                     }})
     
-    observeEvent(input$nex,{
-        output$test <- NULL
-        
-        output$stimuli <- renderPrint({rand_df[input$nex]})
-        answers[[input$nex]] <<- input$ans
-        # output$nex <- renderPrint({
-        #     paste(input$nex, answers[[input$nex]])
-        # })
-        output$allans <- renderPrint({answers})
-        })
+    observeEvent(tohide2(),{
+        print("updating params")
+        print(tohide2())
+        params$counter <- params$counter + 1
+        params$counter <- ifelse(params$counter > 0, 1, 0)})
+    
+    
 }
 
 # Run the application 
